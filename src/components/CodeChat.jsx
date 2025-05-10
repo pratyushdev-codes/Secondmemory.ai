5a819095f90f9aafaf080e4897d3e0437b7507b1
Pratyush Birole  ▲ <birole.pratyush@gmail.com>
2025-05-10 09:24:18 +0530

    Update CodeChat.jsx , fixing Codechat AI icon

--- src/components/CodeChat.jsx
+++ src/components/CodeChat.jsx
@@ -10,7 +10,7 @@ const LoadingMessage = () => (
     <div className="w-8 h-8 rounded-full flex-shrink-0">
       <img
         className="w-8 h-8 rounded-full opacity-60"
-        src="../assets/AIAvatar.png"
+         src="/images/AIAvatar.png"
         alt="AI Avatar"
       />
     </div>
